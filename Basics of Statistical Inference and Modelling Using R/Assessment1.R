853b4a863ad08aa342a7c55aaac55529d5944424
karxth <karunashrestha824@gmail.com>
2021-01-08 10:47:04 +0545

    Completed Assessment1

--- Basics of Statistical Inference and Modelling Using R/Assessment1.R
+++ Basics of Statistical Inference and Modelling Using R/Assessment1.R
@@ -1,16 +1,17 @@
 dataset <- read.csv("datasets/Eucalyptus1.csv")
-
-# Generate 1000 samples of size n=5 to produce a sampling distribution for the 
-# sample mean x
 sample_means <- numeric(1000)
 sample_medians <- numeric(1000)
 sample_sd <- numeric(1000)
 
+# Generate 1000 samples of size n=5 to produce a sampling distribution for the 
+#------------ FOR SAMPLE SIZE N = 5 ------------#
+# sample mean x
+
 for(i in 1:1000){
-  sample1 <- sample(dataset$hgt, size = 5, replace = FALSE)
+sample1 <- sample(dataset$hgt, size = 5, replace = FALSE)
   sample_means[i] <- mean(sample1)  
 }
-hist(sample_means)
+hist(sample_means, col = c("blue3", "blue1"))
 
 #Sample median
 
@@ -28,7 +29,7 @@ for (i in 1:1000){
 }
 hist(sample_sd)
 
-# For N = 20
+#------------ FOR SAMPLE SIZE N = 20 ------------#
 for(i in 1:1000){
   sample1 <- sample(dataset$hgt, size = 20, replace = FALSE)
   sample_means[i] <- mean(sample1)  
@@ -51,25 +52,25 @@ for (i in 1:1000){
 }
 hist(sample_sd)
 
-# tree taller than 15
+#----------FOR TREE TALLER THAN 15M------------#
 tree_tall_15 <- dataset[dataset$hgt > 15,]
 tree_tall_15
 for (i in 1:1000){
-  sample1 = sample(tree_tall_15$hgt, size = 20, replace = FALSE)
+  sample1 = sample(tree_tall_15$hgt, size = 5, replace = FALSE)
   sample_means[i] <- mean(sample1)
 }
 hist(sample_means)
 
 # median
 for (i in 1:1000){
-  sample1 = sample(tree_tall_15$hgt, size = 20, replace = FALSE)
-  sample_medians[i] <- medians(sample1)
+  sample1 = sample(tree_tall_15$hgt, size = 5, replace = FALSE)
+  sample_medians[i] <- median(sample1)
 }
 hist(sample_medians)
 
 # standard deviation 
 for (i in 1:1000){
-  sample1 = sample(tree_tall_15$hgt, size = 20, replace = FALSE)
+  sample1 = sample(tree_tall_15$hgt, size = 5, replace = FALSE)
   sample_sd[i] <- sd(sample1)
 }
 hist(sample_sd)
